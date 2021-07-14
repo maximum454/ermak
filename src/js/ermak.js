87fa8ac40c3fff3f97c84bb58a737fe332ce22fc
maxim <maximum454@gmail.com>
2021-07-14 17:47:11 +0600

    fix

--- src/js/ermak.js
+++ src/js/ermak.js
@@ -1,5 +1,23 @@
 
 
 $(function () {
+    const swiper = new Swiper('.swiper-container', {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
 
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 })
